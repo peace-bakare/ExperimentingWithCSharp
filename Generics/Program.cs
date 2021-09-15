8300cab157829cf37d32831b13bc4c03f54189eb
Bakare Peace <peaceultimatum41@outlook.com>
2021-09-15 15:30:46 +0100

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 100;
+            int num2 = 200;
+            Console.WriteLine("num1: " + num1);
+            Console.WriteLine("num2: " + num2);
+
+            Swap<int>(ref num1, ref num2);
+            Console.WriteLine("num1: " + num1);
+            Console.WriteLine("num2: " + num2);
+
+            string str1 = "Sam";
+            string str2 = "Peace";
+            Console.WriteLine("str1: " + str1);
+            Console.WriteLine("str2: " + str2);
+
+            Swap<string>(ref str1, ref str2);
+            Console.WriteLine("str1: " + str1);
+            Console.WriteLine("str2: " + str2);
+        }
+
+        static void Swap<T>(ref T val1, ref T val2)
+        {
+            T temp;
+            temp = val1;
+            val1 = val2;
+            val2 = temp;
+        }
+    }
+}
