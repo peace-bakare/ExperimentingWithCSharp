b33a5d0e1459657f172acb74c1aa6693cede119e
Bakare Peace <peaceultimatum41@outlook.com>
2021-09-21 15:52:31 +0100

    Class Multiples with IsMultipleOf3 implemented

---
+++ Arrays/Multiples.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Arrays
+{
+    public class Multiples
+    {
+        public static int IsMultipleOf3 (int number)
+        {
+            int odd_count = 0;
+            int even_count = 0;
+
+            if (number < 0)
+                number = -number;
+            if (number == 0)
+                return 1;
+            if (number == 1)
+                return 0;
+
+            while (number != 0)
+            {
+                if ((number & 1) != 0)
+                {
+                    odd_count++;
+                }
+
+                if ((number & 2) != 0)
+                {
+                    even_count++;
+                }
+
+                number = number >> 2;
+            }
+
+            return IsMultipleOf3(Math.Abs(odd_count - even_count));
+        }
+    }
+}
