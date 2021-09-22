036c28af24a1c6f6f2527ba0ca6b4a3b2d596738
Bakare Peace <peaceultimatum41@outlook.com>
2021-09-22 14:16:33 +0100

    Added a project to sum up numbers in an array.

---
+++ SimpleArraySum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SimpleArraySum
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 10, 11 };
+
+            Console.WriteLine("Sum of the array is: ", simpleArraySum(array));
+        }
+
+        public static int simpleArraySum (int [] array){
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+    }
+}
